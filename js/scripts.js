111b832f50d043b4f795f7afe02b2cf351c99873
Patricia Hall <patricialynnhall@gmail.com>
2016-02-13 22:25:39 -0800

    hid images using CSS with id tags

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,9 @@
 $(document).ready(function() {
+  var hotWeather = confirm("Do you like hot, dry weather? Click OK for yes or Cancel for no.");
+  if (hotWeather) {
+    $('#hot').show();
+  } else {
+    $('#hot').hide();
+  }
+});
+var
