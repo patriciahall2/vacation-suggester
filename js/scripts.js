3cc9dc96e3fb57d6217e6fe73cee0769799977d5
Patricia <patricialynhall@gmail.com>
2016-02-23 16:30:54 -0800

    addedbootstrap radio form to first question to test layout

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,15 @@
 $(document).ready(function() {
-  
-}
+  $("#hawaii, #jtree, #gorge, #losangeles").hide();
+  $("#userInput").submit(function(event) {
+  $("#hawaii, #jtree, #gorge, #losangeles").hide();
+
+  var nature = $("input:radio[name=nature]:checked" ).val();
+  var city = $("input:radio[name=city]:checked").val();
+  var trek = $("input:radio[name=trek]:checked").val();
+  var culture = $("input:radio[name=culture]:checked").val();
+
+
+
+event.preventDefault();
+ });
+});
