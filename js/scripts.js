385e75a5b1e5b995eadf627b3bb36eac582db5c1
Patricia <patricialynhall@gmail.com>
2016-03-05 19:29:52 -0800

    start to replace radio buttons with drop downs

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,17 @@
 $(document).ready(function() {
+  // $("form#radio-form button").click(function(event){
   $("#hawaii, #jtree, #gorge, #losangeles").hide();
-  $("#userInput").submit(function(event) {
   $("#hawaii, #jtree, #gorge, #losangeles").hide();
+  $("form#userInput button").click(function(event) {
 
-  var nature = $("input:radio[name=nature]:checked" ).val();
-  var trees = $("input:radio[name=city]:checked").val();
-  var trek = $("input:radio[name=trek]:checked").val();
-  var culture = $("input:radio[name=culture]:checked").val();
+    var setting1 = $("select#setting1").val();
+    var trees = $("input:radio[name=city]:checked").val();
+    var trek = $("input:radio[name=trek]:checked").val();
+    var culture = $("input:radio[name=culture]:checked").val();
+
+if (setting1 === "nature") {
+  $("#gorge").show();
+};
 
 
 
